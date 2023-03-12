c13eb6fb3a67b7b4422c3a3557dfa4a8570f3294
Kordian Galinski <31996030+kordiangalinski@users.noreply.github.com>
2023-03-12 10:04:23 +0100

    different approach

--- src/net/kordian/angelia/Main.java
+++ src/net/kordian/angelia/Main.java
@@ -3,18 +3,18 @@
  */
 package net.kordian.angelia;
 
+import java.io.IOException;
+
 /**
  * @author Kordian Galiński
- *
+ * @version 1.0.0
  */
 public class Main {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO: Auto-generated method stub
 		System.out.println("Hello World!");
+		
 	}
 
 }
